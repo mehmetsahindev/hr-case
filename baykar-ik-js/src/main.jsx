5390b49f3a3890f2629d99f41fc8d9787ba72224
Mehmet Şahin <msahin8097@gmail.com>
2024-01-17 02:50:22 +0300

    tailwind and RTK Query setup

--- baykar-ik-js/src/main.jsx
+++ baykar-ik-js/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./tailwind.css";
 import store from "./store.js";
 import { Provider } from "react-redux";
 
